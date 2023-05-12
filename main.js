6546ae5717f8fc5f2bea78f56d90a60dc7eda9df
Ninotro <ninotroia96@gmail.com>
2023-05-12 15:59:09 +0200

    array collaboratori

---
+++ main.js
@@ -0,0 +1,48 @@
+// Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+// Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+// Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+// Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+// Scott Estrada	Developer	scott-estrada-developer.jpg
+// Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg
+
+const collaboratori = [
+    {
+    "nome" : "Wayne Barnett" ,
+    "ruolo" : "Founder & CEO" ,
+    "immagine" : "wayne-barnett-founder-ceo.jpg"
+    } ,
+
+    {
+    "nome" : " Angela Caroll" ,
+    "ruolo" : "Chief Editor" ,
+    "immagine" : "angela-caroll-chief-editor.jpg"
+    } ,
+
+    {
+    "nome" : " Walter Gordon" ,
+    "ruolo" : "Social Media Manager" ,
+    "immagine" : "walter-gordon-office-manager.jpg"
+    } ,
+
+    {
+    "nome" : " Angela Lopez" ,
+    "ruolo" : "Office Manager" ,
+    "immagine" : "angela-lopez-social-media-manager.jpg"
+    } ,
+    {
+    "nome" : " Scott Estrada" ,
+    "ruolo" : "Developer" ,
+    "immagine" : "scott-estrada-developer.jpg"
+    } ,
+    {
+    "nome" : " Barbara Ramos" ,
+    "ruolo" : "Graphic Designer" ,
+    "immagine" : "barbara-ramos-graphic-designer.jpg"
+    } ,
+
+
+
+
+]
+
+console.log(collaboratori);
